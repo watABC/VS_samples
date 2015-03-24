957f08478c813a6f492e4fe9be13c8889631f858
watABC <wat@jirokichi.co.jp>
2015-03-24 16:49:47 +0900

    最初のコード
    エンプティメソッド、側だけ。

---
+++ empty.pas
@@ -0,0 +1,15 @@
+(* -------------------------------
+	関数: 
+	機能: 
+	29141107 wat
+------------------------------- *)
+PROCEDURE myPrg;
+VAR
+	gCnt: integer;
+
+BEGIN {===== MAIN =====}
+
+	AlrtDialog( '実行しました。' );
+	
+END; 
+RUN( myPrg );
